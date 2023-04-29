5fe02e8296e5349ac397a2f1850c759880bb45fb
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-29 18:08:13 +0530

    initial commit

--- department-service/src/main/java/in/gmsk/controller/DepartmentController.java
+++ department-service/src/main/java/in/gmsk/controller/DepartmentController.java
@@ -1,2 +1,39 @@
-package in.gmsk.controller;public class DepartmentController {
+package in.gmsk.controller;
+
+import in.gmsk.entity.Department;
+import in.gmsk.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Department.class);
+
+    @Autowired
+    private DepartmentRepository repository;
+
+    @PostMapping("/save")
+    public Department add(@RequestBody Department department){
+        LOGGER.info("Department save : {}", department);
+        return repository.addDepartment(department);
+    }
+
+    @GetMapping("/fetchAll")
+    public List<Department> findAll(){
+        LOGGER.info("Department findAll");
+        return repository.findAll();
+    }
+
+    @GetMapping("/findById/{id}")
+    public Department findById(@PathVariable Long id){
+        LOGGER.info("Department findById : {}", id);
+        return repository.findById(id);
+    }
 }
