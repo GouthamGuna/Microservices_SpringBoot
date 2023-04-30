3a873a1afe2c6d469573158ebb1a50d9a90072ec
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-30 13:04:41 +0530

    initial commit

--- employee-service/src/main/java/in/gmsk/controller/EmployeeController.java
+++ employee-service/src/main/java/in/gmsk/controller/EmployeeController.java
@@ -1,2 +1,46 @@
-package in.gmsk.controller;public class EmployeeController {
+package in.gmsk.controller;
+
+import in.gmsk.entity.Employee;
+import in.gmsk.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(EmployeeController.class);
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @PostMapping("/save")
+    public Employee add(@RequestBody Employee employee){
+        LOGGER.info("Employee save : {}", employee);
+        return repository.add(employee);
+    }
+
+    @GetMapping("/fetchAll")
+    public List<Employee> findAll(){
+        LOGGER.info("Employee findAll");
+        return repository.findAll();
+    }
+
+    @GetMapping("/findById/{id}")
+    public Employee findById(@PathVariable Long id){
+        LOGGER.info("Employee findById : {}", id);
+        return repository.findById(id);
+    }
+
+    @GetMapping("/findByDepartmentId/{departmentId}")
+    public List<Employee> findByDepartmentId(@PathVariable Long departmentId){
+        LOGGER.info("Employee findByDepartmentId : {}", departmentId);
+        return repository.findByDepartmentId(departmentId);
+    }
+
 }
--- employee-service/src/main/java/in/gmsk/repository/EmployeeRepository.java
+++ employee-service/src/main/java/in/gmsk/repository/EmployeeRepository.java
@@ -1,2 +1,41 @@
-package in.gmsk.repository;public class EmployeeRepository {
+package in.gmsk.repository;
+
+import in.gmsk.entity.Employee;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+
+    private List< Employee > employees = new ArrayList<>();
+
+    @PostMapping("/save")
+    public Employee add(@RequestBody Employee employee){
+        employees.add(employee);
+        return employee;
+    }
+
+    public Employee findById(Long id){
+        return employees.stream()
+                .filter(employee ->
+                        employee.id().equals(id))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public List<Employee> findAll(){
+        return employees;
+    }
+
+    public List<Employee> findByDepartmentId(Long departmentId){
+        return employees.stream()
+                .filter(depId ->
+                        depId.departmentId().equals(departmentId))
+                .toList();
+    }
+
 }
