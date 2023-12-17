595d4939cb328d14819c4729bc0649c82cf52a3e
GouthamGuna <ggowthamsankar@gmail.com>
2023-12-17 10:11:24 +0530

    Upate StaffControllerTest

--- gmsk_official_works/reference_work_api/src/test/java/in/dev/gmsk/controller/StaffControllerTest.java
+++ gmsk_official_works/reference_work_api/src/test/java/in/dev/gmsk/controller/StaffControllerTest.java
@@ -1,4 +1,27 @@
+package in.dev.gmsk.controller;
+
+import in.dev.gmsk.model.Staff;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class StaffControllerTest {
-  
+    @Autowired
+    private StaffController staffController;
+
+    @Test
+    void getStaffPeriodsDetails() {
+
+        List<Staff> staffPeriodsDetails =
+                staffController.getStaffPeriodsDetails();
+
+        assertNotNull( staffPeriodsDetails );
+
+        System.out.println( "staffPeriodsDetails = " + staffPeriodsDetails );
+    }
 }
