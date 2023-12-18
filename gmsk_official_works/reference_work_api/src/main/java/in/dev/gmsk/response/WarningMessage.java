b4fe1127b10105de81d3b8f5d2e088ee024668f6
GouthamGuna <ggowthamsankar@gmail.com>
2023-12-18 21:20:25 +0530

    Update WarningMessage.java

--- gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/response/WarningMessage.java
+++ gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/response/WarningMessage.java
@@ -1,2 +1,16 @@
-package in.dev.gmsk.response;public class WarningMessage {
+package in.dev.gmsk.response;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class WarningMessage {
+
+    private String message;
+    private String values;
+
+    public WarningMessage(String message, String values) {
+        this.message = message;
+        this.values = values;
+    }
 }
