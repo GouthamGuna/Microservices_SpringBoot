dd5346b32c500ae620c6cd044b2062cfdb289259
GouthamGuna <ggowthamsankar@gmail.com>
2023-12-18 21:19:05 +0530

    Update Staff Controller

--- gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/controller/StaffController.java
+++ gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/controller/StaffController.java
@@ -1,9 +1,13 @@
 package in.dev.gmsk.controller;
 
 import in.dev.gmsk.model.Staff;
+import in.dev.gmsk.response.WarningMessage;
 import in.dev.gmsk.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,15 @@ public class StaffController {
     @GetMapping("/period-list")
     public List<Staff> getStaffPeriodsDetails(){
         return staffService.getStaffPeriodsDetails().toList();
+    }
+
+    @GetMapping("/time-sheet")
+    public ResponseEntity<?> getStaffTimeSheetById(@PathVariable String staffId,
+                @PathVariable String locationId, @PathVariable String academicYearId){
+
+        return new ResponseEntity<>(
+                staffService.getStaffTimeSheetById(staffId,
+                        locationId, academicYearId).toList(), HttpStatus.OK
+        );
     }
 }
