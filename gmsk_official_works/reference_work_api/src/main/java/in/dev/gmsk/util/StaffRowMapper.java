e166f1a4d7afb5a763f68ce44e7fae3136b78b16
GouthamGuna <ggowthamsankar@gmail.com>
2023-12-18 21:21:39 +0530

    Update StaffRowMapper

--- gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/util/StaffRowMapper.java
+++ gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/util/StaffRowMapper.java
@@ -1,2 +1,25 @@
-package in.dev.gmsk.util;public class StaffRowMapper {
+package in.dev.gmsk.util;
+
+import in.dev.gmsk.model.Staff;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StaffRowMapper implements RowMapper<Staff> {
+    @Override
+    public Staff mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Staff staff = new Staff();
+        staff.setTimeTableId( rs.getInt( 1 ) );
+        staff.setStaffName( rs.getString( "staffName" ) );
+        staff.setDayName( rs.getString( "dayname" ) );
+        staff.setParticular( rs.getString( "particular" ) );
+        staff.setClassName( rs.getString( "classname" ) );
+        staff.setSpecializationName( rs.getString( "spec" ) );
+        staff.setSectionName( rs.getString( "sectionname" ) );
+        staff.setSubjectName( rs.getString( "subjectname" ) );
+
+        return staff;
+    }
 }
