4659fc832206436a788228b566c2bff0e0e5af38
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-17 21:05:20 +0530

    Update StudentTest.java

--- gmsk_official_works/reference_work_api/src/test/java/in/dev/gmsk/repository/StudentTest.java
+++ gmsk_official_works/reference_work_api/src/test/java/in/dev/gmsk/repository/StudentTest.java
@@ -1,4 +1,35 @@
+package in.dev.gmsk.repository;
+
+import in.dev.gmsk.model.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class StudentTest {
-  
+    @Autowired
+    private StudentRepo StudentRepo;
+
+    @Test
+    void testStudent() {
+
+        Student student = Student.builder().build();
+        student.setFirstName( "First Name" );
+        student.setLastName( "Last Name" );
+        student.setDob( "DOB" );
+        student.setGender( "Male" );
+        student.setAdmissionNo( "12345" );
+        student.setEmailId( "name@gmsk.in" );
+        student.setAddress( "SLM" );
+        student.setMobileNo( "9876543210" );
+        student.setFatherName( "Father Name" );
+        student.setMotherName( "Mother Name" );
+
+        Student save = StudentRepo.save( student );
+        System.out.println( "save = " + save );
+
+        assertTrue( true );
+    }
 }
