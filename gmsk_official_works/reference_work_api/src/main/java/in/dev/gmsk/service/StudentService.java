33ae212cf4e3922446b40d49833720d0e11c844b
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-17 21:04:15 +0530

    Update StudentControllerTest.java

--- gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/service/StudentService.java
+++ gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/service/StudentService.java
@@ -1,2 +1,16 @@
-package in.dev.gmsk.service;public interface StudentService {
+package in.dev.gmsk.service;
+
+import in.dev.gmsk.model.Student;
+
+import java.util.stream.Stream;
+
+public interface StudentService {
+
+    Student saveStudent(Student student);
+
+    Stream<Student> allStudentList();
+
+    Student findByIdStudent(long studentId);
+
+    Student updateStudent(long studentId, Student student);
 }
