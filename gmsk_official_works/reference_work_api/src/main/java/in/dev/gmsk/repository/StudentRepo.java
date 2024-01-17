812ba7e98962f93c0404ef47052bc4eedd0e1d9a
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-17 21:03:04 +0530

    Update StudentRepo.java

--- gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/repository/StudentRepo.java
+++ gmsk_official_works/reference_work_api/src/main/java/in/dev/gmsk/repository/StudentRepo.java
@@ -1,2 +1,9 @@
-package in.dev.gmsk.repository;public interface Student {
+package in.dev.gmsk.repository;
+
+import in.dev.gmsk.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Long> {
 }
